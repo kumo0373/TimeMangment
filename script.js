41c01fdf35f89c50551d418cfbeec057006ce129
kumo0373 <ibrahimahmed0373@gmail.com>
2024-10-28 19:48:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(error => {
-            console.error('Service Worker registration failed:', error);
-        });
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register(https://kumo0373.github.io/TimeMangment/service-worker.js)
+            .then((registration) => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch((error) => {
+                console.error('Service Worker registration failed:', error);
+            });
+    });
 }

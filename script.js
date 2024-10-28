0d4c40e78311df50a7b879817335095c7656994c
kumo0373 <ibrahimahmed0373@gmail.com>
2024-10-28 19:41:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then((registration) => {
-          console.log('ServiceWorker registered with scope:', registration.scope);
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+            console.log('Service Worker registered with scope:', registration.scope);
         })
-        .catch((error) => {
-          console.log('ServiceWorker registration failed:', error);
+        .catch(error => {
+            console.error('Service Worker registration failed:', error);
         });
-    });
-  }
-  
+}
